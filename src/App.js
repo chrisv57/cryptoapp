1d0ce07cd062c1892a693cc6fbd9deaf5a60a572
chrisv57 <verma_c@outlook.com>
2022-01-18 18:00:20 -0500

    Navigation Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
+import { Navbar, Exchanges, CryptoDetails, Cryptocurrencies, News, Homepage} from './components';
+import './App.css';
 const App = () => {
-  return (
-    <div className='app'>
-          <div className='navbar'></div>
-          <div className='main'></div>
-          <div className='footer'></div>
-    </div>
-  
-  )
+    return (
+        <div className='app'>
+            <div className='navbar'>
+                <Navbar />
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route path="/" element={<Homepage />} />
+                            <Route path="/exchanges" element={<Exchanges />} />
+                            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                            <Route path="/crypto/:coindId" element={<CryptoDetails />} />
+                            <Route path="/news" element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
+            </div>
+            <div className='footer'></div>
+        </div>
+    )
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,33 @@ import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
-const Navbar = () => {
-  return (
-<div className='nav-container'>
-    <div className='logo-container'>
-        <Avatar />
-        <Typography.Title level={2} className='logo'>
-            <Link to="/">Cryptoverse</Link>
-        </Typography.Title>
-        {/* <Button className='menu-control-container'>
+import icon from '../images/logo.png';
 
-        </Button> */}
-    </div>
-</div>
-  )
+const Navbar = () => {
+    return (
+        <div className='nav-container'>
+            <div className='logo-container'>
+                <Avatar src={icon} size="large" />
+                <Typography.Title level={2} className='logo'>
+                    <Link to="/">Cryptoverse</Link>
+                </Typography.Title>
+            </div>
+            <Menu theme='dark' className='menu-container'>
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
+        </div>
+    )
 };
 
 export default Navbar;
