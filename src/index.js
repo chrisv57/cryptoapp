507acf54828080a5ed8a1517df24b7c01ba7804a
chrisv57 <verma_c@outlook.com>
2022-01-18 19:02:11 -0500

    implement a reducer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import store from './App/store';
 import 'antd/dist/antd.css';
 ReactDom.render(
     <Router>
-        <App />
+    {/* Now everysingle component inside the app will have access to the store variable by putting it under the provider */}
+        <Provider store={store}>
+            <App /> 
+        </Provider>
+
     </Router>,
     document.getElementById('root')
 );
