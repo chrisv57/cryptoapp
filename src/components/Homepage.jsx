7848f971305d9f655e20d310b4a8d2abd3fec2f8
chrisv57 <verma_c@outlook.com>
2022-01-18 21:38:19 -0500

    Show all currencies

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,46 +1,75 @@
-import React from 'react';
-import millify from 'millify'; //Converts long numbers into pretty, human-readable strings.
-import { Typography, Row, Col, Statistic } from 'antd';
-import { Link } from 'react-router-dom';
+import React from "react";
+import millify from "millify"; //Converts long numbers into pretty, human-readable strings.
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
 
-import { useGetCryptosQuery } from '../services/cryptoApi';
-import { News,Cryptocurrencies } from '../components';
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { News, Cryptocurrencies } from "../components";
 
 const { Title } = Typography;
 
-
-
 const Homepage = () => {
+  //getting data using reducer hook
+  const { data, isFetching } = useGetCryptosQuery(10);
 
-    //getting data using reducer hook
-    const { data, isFetching } = useGetCryptosQuery(10);
+  //to use data
+  const globalStats = data?.data?.stats;
 
-    //to use data
-    const globalStats = data?.data?.stats;
+  if (isFetching) return "Loading...";
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total market cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total markets"
+            value={millify(globalStats.totalMarkets)}
+          />
+        </Col>
+      </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Cryptocurrencies News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
+    </>
+  );
+};
 
-    if(isFetching) return 'Loading...';
-    return (
-        <>
-            <Title level={2} className='heading'>Global Stats</Title>
-            <Row>
-                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
-                <Col span={12}><Statistic title="Total market cap" value={millify(globalStats.totalMarketCap)} /></Col>
-                <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
-                <Col span={12}><Statistic title="Total markets" value={millify(globalStats.totalMarkets)} /></Col>
-            </Row>
-            <div className="home-heading-container">
-                <Title level={2} className='home-title'>Top 10 Cryptocurrencies</Title>
-                <Title level={3} className='show-more'><Link to = "/cryptocurrencies">Show More</Link></Title>
-            </div>
-            <Cryptocurrencies simplified />
-            <div className="home-heading-container">
-                <Title level={2} className='home-title'>Cryptocurrencies News</Title>
-                <Title level={3} className='show-more'><Link to = "/news">Show More</Link></Title>
-            </div>
-            <News simplified/>
-        </>
-    )
-}
-
-export default Homepage
+export default Homepage;
